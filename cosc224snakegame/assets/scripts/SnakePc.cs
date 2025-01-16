724558d74a0f6859bf0cf5c2b9e54e5085fc9ca5
Jesse H <hetjess@gmail.com>
2025-01-16 10:45:03 -0800

    Finished player movement including last known dir.

--- cosc224snakegame/assets/scripts/SnakePc.cs
+++ cosc224snakegame/assets/scripts/SnakePc.cs
@@ -3,36 +3,89 @@ using System;
 
 public partial class SnakePc : CharacterBody2D
 {
-	private int lastDirection;
+	private int lastDirection; //0 - not moving, 1 - up, 2 - right, 3 - down, 4 - left
 	private Vector2 playerPosition;
 	[Export]
 	public int Speed { get; set; } = 400;
-	public void GetInput()
+	[Export] private float delaySeconds = 1f;
+	[Export] private float moveTimer = 0f;
+	
+	public Vector2 GetInput()
 	{
 		// Add these actions in Project Settings -> Input Map.
 		Vector2 inputDir = Input.GetVector("move-left", "move-right", "move-up", "move-down");
 		Velocity = inputDir * Speed;
+		GD.Print(GlobalPosition);
+		return inputDir;
 	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		lastDirection = 0;
-		//playerPosition = new Vector2(8,8);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		//Set last direction here:
+		Vector2 dir = GetInput();
+		if(dir.X != 0)
+		{
+			if(dir.X > 0)
+			{
+				lastDirection = 2;
+			}
+			else
+			{
+				lastDirection = 4;
+			}
+		}
+		else if(dir.Y != 0)
+		{
+			if(dir.Y > 0)
+			{
+				lastDirection = 1;
+			}
+			else
+			{
+				lastDirection = 3;
+			}
+		}
 	}
 	public override void _PhysicsProcess(double delta){
-	
-		GetInput();
-		//var dir = GlobalPosition;
-		// Don't move if too close to the mouse pointer.
-		
-		//Rotation = dir.Angle();
-		MoveAndSlide();
-		
+		if(moveTimer < delaySeconds)
+		{
+			moveTimer += (float)delta;
+			return;
+		}
+		else
+		{
+			moveTimer = 0f;
+		}
+		Vector2 dir = GetInput();
+		int pixels = 16;
+		float playerX = GlobalPosition.X;
+		float playerY = GlobalPosition.Y;
+			//auto move
+		switch(lastDirection){
+			case 1:
+				playerY += 16;
+				this.SetGlobalPosition(new Vector2(playerX, playerY));
+				break;
+			case 2:
+				playerX += 16;
+				this.SetGlobalPosition(new Vector2(playerX, playerY));
+				break;
+			case 3:
+				playerY -= 16;
+				this.SetGlobalPosition(new Vector2(playerX, playerY));
+				break;
+			case 4:
+				playerX -= 16;
+				this.SetGlobalPosition(new Vector2(playerX, playerY));
+				break;
+			default:
+				break;
+		}
 	}
 }
