f543ae23a2a499bfcebc303a57ab2ab6a32f174e
Jesse H <hetjess@gmail.com>
2025-01-16 09:58:04 -0800

    Adding tile position for player and fixing file system

--- cosc224snakegame/assets/scripts/SnakePc.cs
+++ cosc224snakegame/assets/scripts/SnakePc.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class SnakePc : CharacterBody2D
 {
+	private int lastDirection;
+	private Vector2 playerPosition;
 	[Export]
 	public int Speed { get; set; } = 400;
 	public void GetInput()
@@ -10,13 +12,12 @@ public partial class SnakePc : CharacterBody2D
 		// Add these actions in Project Settings -> Input Map.
 		Vector2 inputDir = Input.GetVector("move-left", "move-right", "move-up", "move-down");
 		Velocity = inputDir * Speed;
-		GD.Print(Velocity);
-		GD.Print(inputDir);
-		GD.Print(GlobalPosition);
 	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		lastDirection = 0;
+		//playerPosition = new Vector2(8,8);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,6 +26,7 @@ public partial class SnakePc : CharacterBody2D
 		
 	}
 	public override void _PhysicsProcess(double delta){
+	
 		GetInput();
 		//var dir = GlobalPosition;
 		// Don't move if too close to the mouse pointer.
