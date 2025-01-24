b76eebb96acdfaa1badb356ed8c81f8c0b93f985
Swambe <sammichaelblair@gmail.com>
2025-01-24 11:56:04 -0800

    Added Complete Musci SoundTrack and sound effects

--- cosc224snakegame/scenes/parallax-scroll.gd
+++
@@ -1,7 +0,0 @@
-extends ParallaxBackground
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	scroll_offset.x -= 200 * delta;
--- cosc224snakegame/scripts/apple_pickup.gd
+++
@@ -1,4 +0,0 @@
-extends Sprite2D
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	$Area2D/Eat.play();
